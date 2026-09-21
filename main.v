2965405e1c57c406d76ba7481fb03a78f3f48171
scher <schermann470@outlook.com>
2026-09-22 01:24:27 +0300

    up

--- main.v
+++ main.v
@@ -161,8 +161,8 @@ fn build_tr_menu(cur_lang Lang) string {
 		'<option value=es ${if cur_lang == .es { 'selected' } else { '' }}>ES</option>' +
 		'<option value=fr ${if cur_lang == .fr { 'selected' } else { '' }}>FR</option>' +
 		'<option value=ja ${if cur_lang == .ja { 'selected' } else { '' }}>日本語</option>' +
-		'<option value=zh ${if cur_lang == .zh { 'selected' } else { '' }}>中文</option></select>' +
-		'<option value=tr ${if cur_lang == .tr { 'selected' } else { '' }}>TR</option>'
+		'<option value=zh ${if cur_lang == .zh { 'selected' } else { '' }}>中文</option>' +
+		'<option value=tr ${if cur_lang == .tr { 'selected' } else { '' }}>TR</option></select>'
 	/*
 	s := match cur_lang {
 		.ru { 'English' }
